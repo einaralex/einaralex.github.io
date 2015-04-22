1d5be01c9567f5d77d6ca5c8efa05bbf5abee2fe
einaralex <einaralex@gmail.com>
2015-04-22 21:17:53 +0000

    some more angular tests

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
 	$routeProvider.when("/", {
 		templateUrl: "templates/home.html",
 		controller: "Controller",
 		title: 'testing'
 	});
+
+	$locationProvider.html5Mode(true);
 });
