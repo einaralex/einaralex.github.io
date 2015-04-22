3102424b3b1c300af7a0bf6845f1b1445db66a13
einaralex <einaralex@gmail.com>
2015-04-22 22:04:38 +0000

    some more angular tests

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider.when("/", {
 		templateUrl: "templates/home.html",
-		controller: "homeController",
-		css: 'templates/styles/home.css'
+		controller: "homeController"
 	});
 
 	$locationProvider.html5Mode({ 	// Removes the hashbang from the url
